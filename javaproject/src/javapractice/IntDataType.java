05cb172a332798bd097b359faa3819cac215ba43
herit6-hanu <hhanumanthareddym2000@gmail.com>
2024-02-07 01:55:01 +0530

    commit the new files to the github for tracking

---
+++ javaproject/src/javapractice/IntDataType.java
@@ -0,0 +1,14 @@
+package javapractice;
+
+public class IntDataType {
+	public static void main(String[] args) {
+		byte a=-24;
+		System.out.println(Integer.toBinaryString(a));
+		int b=0b11111111111111111111111111101000;
+		System.out.println(b);
+		int c=0x65;
+		System.out.println(c);
+		
+	}
+
+}
