495f3519450f4b155fbadbcdeaa3e28539de71c5
herit6-hanu <hhanumanthareddym2000@gmail.com>
2024-02-08 23:03:37 +0530

    add the latest files & folders to hub

---
+++ javaproject/src/javapractice/TypeCasting.java
@@ -0,0 +1,13 @@
+package javapractice;
+
+public class TypeCasting {
+	public static void main(String[] args) {
+		byte a=56;
+		int b;
+		//here we are assigning the value of a(byte) to b(int)
+		//converting the one data type to another data type is known as type casting
+		b=a;
+		System.out.println(b);
+	}
+
+}
---
+++ javaproject/src/javapractice/TypeCasting2.java
@@ -0,0 +1,12 @@
+package javapractice;
+
+public class TypeCasting2 {
+	public static void main(String[] args) {
+		int a=272;
+		byte b;
+		b=(byte) a;
+		System.out.println(b);
+		
+	}
+
+}
