236721167c5bb7375ca629314e8cbc0c41e906a2
herit6-hanu <hhanumanthareddym2000@gmail.com>
2024-02-11 22:18:46 +0530

    commit the files

--- javaproject/src/javapractice/ReturnMain.java
+++ javaproject/src/javapractice/ReturnMain.java
@@ -3,6 +3,10 @@ package javapractice;
 public class ReturnMain {
 	public static void main(String[] args) {
 		System.out.println("Inside the main method");
+		/*
+		 * Not possible to make the main method as int or string or any other
+		 * because the jvm don't expect anything from main method
+		 */
 	}
 
 }
