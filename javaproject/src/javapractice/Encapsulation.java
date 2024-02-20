ad45f0a2c37bf7a8288c16656b795bff9b86ecfd
Hanumantha Reddy <@hhanumanthareddym2000@gmail.com>
2024-02-21 00:55:17 +0530

    created new folder with the name javascript to practice javascript

--- javaproject/src/javapractice/Encapsulation.java
+++ javaproject/src/javapractice/Encapsulation.java
@@ -2,32 +2,36 @@ package javapractice;
 
 public class Encapsulation {
 	public static void main(String[] args) {
-		En e=new En();
+		En e = new En();
 		e.setAccno(919100412870l, 9182, 24);
 		System.out.println(e.getAccno());
 		System.out.println(e.getPkey());
 		System.out.println(e.getBal());
 	}
-	
+
 }
+
 class En {
 	private long accno;
 	private int pkey;
 	private int bal;
+
 	public long getAccno() {
 		return accno;
 	}
-	public void setAccno(long accno,int pkey,int bal) {
-		accno = accno;
-		System.out.println(accno);
-		pkey=pkey;
-		bal=bal;
+
+	public void setAccno(long accno, int pkey, int bal) {
+		this.accno = accno;
+		this.pkey = pkey;
+		this.bal = bal;
 	}
+
 	public int getPkey() {
 		return pkey;
 	}
+
 	public int getBal() {
 		return bal;
 	}
-	
+
 }
