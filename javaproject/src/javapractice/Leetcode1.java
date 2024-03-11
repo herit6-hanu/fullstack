ecc224e562e54d8283a999a3da2f3eca9ee38e69
herit6-hanu <hhanumanthareddym2000@gmail.com>
2024-03-12 01:49:38 +0530

    add the files to git

---
+++ javaproject/src/javapractice/Leetcode1.java
@@ -0,0 +1,21 @@
+package javapractice;
+
+public class Leetcode1 {
+	public static int[] plusOne(int[] digits) {
+        String s="";
+        for(int i=0;i<digits.length;i++){
+           int temp=digits[i];
+           s+=Integer.toString(temp);
+        }
+        int integer=Integer.parseInt(s);
+        integer++;
+        System.out.println(integer);
+        int[] res=new int[0];
+        return res;
+    }
+	public static void main(String[] args) {
+		int[] arr= {1,2,3};
+		plusOne(arr);
+	}
+
+}
