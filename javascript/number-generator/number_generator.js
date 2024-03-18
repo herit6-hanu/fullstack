6d539f6d04450a9a34c20feb0adb1cc67b2f7f9c
herit6-hanu <hhanumanthareddym2000@gmail.com>
2024-03-19 00:01:13 +0530

    done

--- javascript/number-generator/number_generator.js
+++ javascript/number-generator/number_generator.js
@@ -1,17 +1,27 @@
 document.getElementById("btn").addEventListener("click", () => {
     let number = Number(document.getElementById("input").value);
-    console.log(number);
+    let resultBody = document.getElementsByClassName("result-body")[0];
+
+    clearChildElements(resultBody);
     let i = 0;
     if (number >0 && number !==NaN) {
         while (i < number) {
-
             i++;
-            
+            let resultBlock = document.createElement("div");
+            resultBlock.className = "results";
+            resultBlock.id = "results";
+            resultBlock.textContent = i;
+            resultBody.appendChild(resultBlock);   
         }
     } else {
-        let error= document.createElement("p");
-        error.textContent="Please provide valid number...";
-        document.appendChild(error);
+        let errorParagraph = document.getElementsByTagName("p")[0];
+        errorParagraph.textContent = "Oops...Please enter valid number.";
+        errorParagraph.style.color = "red";
     }
-    
 })
+
+function clearChildElements(parentElement){
+    while (parentElement.firstChild) {
+        parentElement.removeChild(parentElement.firstChild);
+    }
+}
