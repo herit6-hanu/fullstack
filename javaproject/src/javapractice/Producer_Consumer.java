fe0ad9e576d6dfb0ac18e7506c7b4a160049ec42
herit6-hanu <hhanumanthareddym2000@gmail.com>
2024-03-30 00:21:17 +0530

    add the files

---
+++ javaproject/src/javapractice/Producer_Consumer.java
@@ -0,0 +1,61 @@
+package javapractice;
+
+public class Producer_Consumer {
+	public static void main(String[] args) {
+		StringBuffer sb=new StringBuffer();
+		Queue q = new Queue();
+		Produce p = new Produce(q);
+		Consume c = new Consume(q);
+		c.start();
+		p.start();
+
+	}
+}
+
+class Queue {
+	int x;
+
+	void produce() {
+		int i = 1;
+
+		for (;;) {
+			x = i;
+			System.out.println("produced " + i++);
+		}
+
+	}
+
+	void consume() {
+		for (;;) {
+			System.out.println("consumed " + x);
+		}
+	}
+}
+
+class Produce extends Thread {
+	Queue q;
+
+	public Produce(Queue q) {
+		// TODO Auto-generated constructor stub
+		this.q = q;
+
+	}
+
+	public void run() {
+		q.produce();
+	}
+}
+
+class Consume extends Thread {
+	Queue q;
+
+	public Consume(Queue q) {
+		// TODO Auto-generated constructor stub
+		this.q = q;
+
+	}
+
+	public void run() {
+		q.consume();
+	}
+}
--- javascript/weather-app/weather.js
+++ javascript/weather-app/weather.js
@@ -3,26 +3,15 @@ window.addEventListener("load", () => {
     navigator.geolocation.getCurrentPosition((position) => {
         lat = position.coords.latitude;
         long = position.coords.longitude;
-        
-        // Replace 'YOUR_API_KEY' with your actual AccuWeather API key
-        const apiKey = 'Fy5RZKOJwiuwXnB8ciiMqAHyz9mASA4v';
-        
-        // Construct the URL for the AccuWeather API endpoint
-        const apiUrl = `https://developer.accuweather.com/accuweather-forecast-api/apis/get/forecasts/v1/hourly/1hour/%7BlocationKey%7Dhttp://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${apiKey}&q=${lat}%20%2C${long}`;
-
-        fetch(apiUrl)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
-            // Handle the weather data here
-        })
-        .catch(error => {
-            console.error('Error:', error);
+        let apiKey = "Fy5RZKOJwiuwXnB8ciiMqAHyz9mASA4v&q";
+        let obj = axios.get(`http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${apiKey}=${lat}%2C${long}`).then((resposne) => {
+            let dataKey = resposne.data.Key;
+            axios.get(`http://dataservice.accuweather.com/forecasts/v1/daily/1day/${dataKey}?apikey=${apiKey}`).then((anotherResponse) => {
+                console.log(anotherResponse);
+            });
         });
+        setTimeout(() => {
+            console.log(obj);
+        },3000)
     });
 });
