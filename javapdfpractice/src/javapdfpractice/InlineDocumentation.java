95535f3eed33536b31e4cab1c18b2af0da2bc326
herit6-hanu <hhanumanthareddym2000@gmail.com>
2024-04-01 00:02:10 +0530

    add the files

---
+++ javapdfpractice/src/javapdfpractice/InlineDocumentation.java
@@ -0,0 +1,14 @@
+package javapdfpractice;
+
+public class InlineDocumentation {
+	//single line comment 
+	//TODO
+	//FIXME
+	public static void main(String[] args) {
+		/**
+		 * This is multi line comment for the main method...
+		 */
+		System.exit(1);
+	}
+
+}
